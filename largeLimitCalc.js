73becf5f5f2458d674533f986a9699e24d47a31f
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-11-11 14:05:21 +0500

    Update largeLimitCalc.js

--- largeLimitCalc.js
+++ largeLimitCalc.js
@@ -19,3 +19,25 @@ class largeLimitCalc
 		return this.result;
 	}
 }
+
+// Suggested by Uni in unitextwall.glitch.me
+class largeLimitCalc_Uni
+{
+	constructor(k,exp)
+	{
+		this.result = 0;
+		if((Math.log10(k)*exp)>=9223372036854775807)
+		{
+			this.result = Infinity; // if it reaches ≥10^9223372036854775807, it will overflow.
+		}
+		else if((Math.log10(k)*exp)>=10)
+		{
+			this.result = Math.pow(10,(Math.log10(k)*exp%1)).toFixed(9) + "E" + Math.floor(Math.log10(k)*exp); // return to scientific notation
+		}
+		else
+		{
+			this.result = k**exp; // k^exp
+		}
+		return this.result;
+	}
+}
