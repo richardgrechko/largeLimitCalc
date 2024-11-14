6fc3b553fb977a572054777faea367fb3f11e165
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-11-14 15:27:28 +0500

    Update largeLimitCalc.js

--- largeLimitCalc.js
+++ largeLimitCalc.js
@@ -56,7 +56,7 @@ largeLimitCalcV2.factorial = function(k)
 	this.result = 0;
 	if(k>1)
 	{
-		this.result = Math.log10(k)+this.factorial(k-1); // return to scientific notation
+		this.result = Math.log10(Math.floor(k))+this.factorial(Math.floor(k-1)); // return to scientific notation
 	}
 	else
 	{
